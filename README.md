77a93fc3ac656d9f138ecf05dfbb5d027d7082b5
Patrick Wade <patrickwadedev@googlemail.com>
2025-04-09 15:13:26 +0200

    improve script example and readme

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
 # How to deploy
 
+_video tutorial: https://www.youtube.com/watch?v=IbJMb-qsgaY_
+
 ### 1. Have a vps ready
 
-### 2. log in by ssh (ssh root@<ip4>)
+### 2. Prepare a public repository with the node application
 
-*optional: update vps*
+### 3. log in by ssh (ssh root@<ip4>)
+
+_optional: update vps_
 - sudo apt update # get updates
 - sudo apt upgrade # upgrade the updates
 
 
-### 3. Install Node (Replace "18" with your desired major version)
+### 4. Install Node (Replace "18" with your desired major version)
 
 `curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -`
 
 `sudo apt install -y nodejs`
+
+### 5. Install git (probably already installed `git -v to check`)
+
+`sudo apt install git`
+
+### 6. clone public repository
+
+_for a private repository either use ssh authentication or create a personal access token for https_
+
+`git clone https://github.com/PWadeDev/node-hetzner-manual.git`
--- src/index.js
+++ src/index.js
@@ -1,23 +1,58 @@
 import { createServer } from "node:http";
+import { parse } from "node:url";
 
 const port = 3000;
 
-const server = createServer((req, res) => {
-  // Set response HTTP headers
-  res.writeHead(200, { "Content-Type": "text/html" });
+const handlers = {
+  GET: {
+    "/": (_req, res) => {
+      console.log("Serving root HTML page");
 
-  // Send basic HTML content
-  res.end(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Hello</title>
-      </head>
-      <body>
-        <h1>Hello World</h1>
-      </body>
-    </html>
-  `);
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Hello</title>
+          </head>
+          <body>
+            <h1>Hello World</h1>
+          </body>
+        </html>
+      `);
+    },
+
+    "/books": (_req, res) => {
+      console.log("Serving book list");
+
+      const books = [
+        { id: 1, title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
+        { id: 2, title: "1984", author: "George Orwell" },
+        { id: 3, title: "To Kill a Mockingbird", author: "Harper Lee" },
+      ];
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(books, null, 2));
+    },
+  },
+};
+
+const server = createServer((req, res) => {
+  const { pathname } = parse(req.url, true);
+  const method = req.method;
+
+  console.log(`Incoming request: ${method} ${pathname}`);
+
+  const routeHandler = handlers[method]?.[pathname];
+
+  if (routeHandler) {
+    routeHandler(req, res);
+  } else {
+    console.log("Route not found");
+
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Not Found" }));
+  }
 });
 
 server.listen(port, () => {
